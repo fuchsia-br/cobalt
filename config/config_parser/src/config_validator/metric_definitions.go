14f3b6152d6b900873f7bd8ecc8ba6c689159828
Alexandre Zani <azani@google.com>
2018-08-28 16:43:11 -0700

    Add config validation.
    * Validate the meta_data field.
    * Validate the event_types field.
    * Some validate for individual metric types.
    
    Change-Id: I2cf96e2fb16eed6d0c2d16182da3f56eff5dfd34

--- config/config_parser/src/config_validator/metric_definitions.go
+++ config/config_parser/src/config_validator/metric_definitions.go
@@ -8,8 +8,14 @@ import (
 	"config"
 	"config_parser"
 	"fmt"
+	"net/mail"
 	"regexp"
+	"time"
 )
+
+// Date format is yyyy/mm/dd. This is done by specifying in the desired format
+// Mon Jan 2 15:04:05 MST 2006. See time module documentation.
+const dateFormat = "2006/01/02"
 
 // This file contains logic to validate lists of MetricDefinition protos.
 
@@ -55,27 +61,26 @@ func validateMetricDefinition(m config.MetricDefinition) (err error) {
 	if m.MetaData == nil {
 		return fmt.Errorf("meta_data is not set.")
 	}
-	validateMetadata(*m.MetaData)
+
+	if err := validateMetadata(*m.MetaData); err != nil {
+		return fmt.Errorf("Error in meta_data: %v", err)
+	}
 
 	if m.MaxEventTypeIndex != 0 && m.MetricType != config.MetricDefinition_EVENT_OCCURRED {
 		return fmt.Errorf("Metric %s has max_event_type_index set. max_event_type_index can only be set for metrics for metric type EVENT_OCCURRED.", m.MetricName)
-	}
-
-	if m.MaxEventTypeIndex >= 1024 {
-		return fmt.Errorf("Metric %s has max_event_type_index = %v. The maximum value for max_event_type_index is 1024.", m.MetricName, m.MaxEventTypeIndex)
 	}
 
 	if m.IntBuckets != nil && m.MetricType != config.MetricDefinition_INT_HISTOGRAM {
 		return fmt.Errorf("Metric %s has int_buckets set. int_buckets can only be set for metrics for metric type INT_HISTOGRAM.", m.MetricName)
 	}
 
-	// TODO(azani): Validate the content of int_buckets.
-
 	if len(m.Parts) > 0 && m.MetricType != config.MetricDefinition_CUSTOM {
 		return fmt.Errorf("Metric %s has parts set. parts can only be set for metrics for metric type CUSTOM.", m.MetricName)
 	}
 
-	// TODO(azani): Validate the content of parts.
+	if err := validateMetricDefinitionForType(m); err != nil {
+		return fmt.Errorf("Metric %s: %v", m.MetricName, err)
+	}
 
 	// TODO(azani): Validate reports.
 	return nil
@@ -83,6 +88,124 @@ func validateMetricDefinition(m config.MetricDefinition) (err error) {
 
 // Validate a single instance of Metadata.
 func validateMetadata(m config.MetricDefinition_Metadata) (err error) {
-	// TODO(azani): Validate metadata.
+	if len(m.ExpirationDate) == 0 {
+		return fmt.Errorf("No expiration_date set.")
+	}
+
+	var exp time.Time
+	exp, err = time.ParseInLocation(dateFormat, m.ExpirationDate, time.UTC)
+	if err != nil {
+		return fmt.Errorf("Invalid expiration_date '%v'. expiration_date must use the yyyy/mm/dd format.", m.ExpirationDate)
+	}
+
+	// Date one year and one day from today.
+	maxExp := time.Now().AddDate(1, 0, 0)
+	if exp.After(maxExp) {
+		return fmt.Errorf("The expiration_date must be set no more than 1 year in the future.")
+	}
+
+	for _, o := range m.Owner {
+		if _, err = mail.ParseAddress(o); err != nil {
+			return fmt.Errorf("'%v' is not a valid email address in owner field")
+		}
+	}
+
+	if m.MaxReleaseStage == config.ReleaseStage_RELEASE_STAGE_NOT_SET {
+		return fmt.Errorf("No max_release_stage set.")
+	}
+
+	return nil
+}
+
+// Validate the event_types and max_event_type_index fields.
+func validateEventTypes(m config.MetricDefinition) error {
+	if len(m.EventTypes) == 0 {
+		return fmt.Errorf("no event_types listed for metric of type %s.", m.MetricType)
+	}
+
+	if m.MaxEventTypeIndex >= 1024 {
+		return fmt.Errorf("max_event_type_index must be less than 1024.")
+	}
+
+	if m.MaxEventTypeIndex == 0 {
+		return nil
+	}
+
+	for i, _ := range m.EventTypes {
+		if i > m.MaxEventTypeIndex {
+			return fmt.Errorf("Event index %v is greater than max_event_type_index %v.", i, m.MaxEventTypeIndex)
+		}
+	}
+
+	return nil
+}
+
+///////////////////////////////////////////////////////////////
+// Validation for specific metric types:
+///////////////////////////////////////////////////////////////
+
+func validateMetricDefinitionForType(m config.MetricDefinition) error {
+	switch m.MetricType {
+	case config.MetricDefinition_EVENT_OCCURRED:
+		return validateEventOccurred(m)
+	case config.MetricDefinition_EVENT_COUNT:
+		return validateEventTypes(m)
+	case config.MetricDefinition_ELAPSED_TIME:
+		return validateEventTypes(m)
+	case config.MetricDefinition_FRAME_RATE:
+		return validateEventTypes(m)
+	case config.MetricDefinition_MEMORY_USAGE:
+		return validateEventTypes(m)
+	case config.MetricDefinition_INT_HISTOGRAM:
+		return validateIntHistogram(m)
+	case config.MetricDefinition_STRING_USED:
+		return validateStringUsed(m)
+	case config.MetricDefinition_CUSTOM:
+		return validateCustom(m)
+	}
+
+	return fmt.Errorf("Unknown MetricType: %v", m.MetricType)
+}
+
+func validateEventOccurred(m config.MetricDefinition) error {
+	if m.MaxEventTypeIndex == 0 {
+		return fmt.Errorf("No max_event_type_index specified for metric of type EVENT_OCCURRED.")
+	}
+
+	return validateEventTypes(m)
+}
+func validateIntHistogram(m config.MetricDefinition) error {
+	if m.IntBuckets == nil {
+		return fmt.Errorf("No int_buckets specified for metric of type INT_HISTOGRAM.")
+	}
+
+	// TODO(azani): Validate bucket definition.
+
+	return validateEventTypes(m)
+}
+
+func validateStringUsed(m config.MetricDefinition) error {
+	if len(m.EventTypes) > 0 {
+		return fmt.Errorf("event_types must not be set for metrics of type STRING_USED")
+	}
+	return nil
+}
+
+func validateCustom(m config.MetricDefinition) error {
+	if len(m.EventTypes) > 0 {
+		return fmt.Errorf("event_types must not be set for metrics of type CUSTOM")
+	}
+
+	if len(m.Parts) == 0 {
+		return fmt.Errorf("No parts specified for metric of type CUSTOM.")
+	}
+
+	for n := range m.Parts {
+		if !validNameRegexp.MatchString(n) {
+			return fmt.Errorf("Invalid part name '%s'. Part names must match the regular expression '%v'", n, validNameRegexp)
+		}
+
+		// TODO(azani): Validate the MetricPart itself.
+	}
 	return nil
 }
--- config/config_parser/src/config_validator/metric_definitions_test.go
+++ config/config_parser/src/config_validator/metric_definitions_test.go
@@ -4,6 +4,7 @@ import (
 	"config"
 	"config_parser"
 	"testing"
+	"time"
 )
 
 // Allows generating a list of MetricTypes for which we can run tests.
@@ -25,18 +26,28 @@ func metricTypesExcept(remove ...config.MetricDefinition_MetricType) (s []config
 	return
 }
 
-func makeValidMetric() (metric config.MetricDefinition) {
+func makeValidMetadata() config.MetricDefinition_Metadata {
+	return config.MetricDefinition_Metadata{
+		ExpirationDate:  time.Now().AddDate(1, 0, 0).Format(dateFormat),
+		Owner:           []string{"google@example.com"},
+		MaxReleaseStage: config.ReleaseStage_DEBUG,
+	}
+}
+
+func makeValidMetric() config.MetricDefinition {
 	return makeValidMetricWithName("the_metric_name")
 }
 
 // makeValidMetric returns a valid instance of config.MetricDefinition which
 // can be modified to fail various validation checks for testing purposes.
-func makeValidMetricWithName(name string) (metric config.MetricDefinition) {
+func makeValidMetricWithName(name string) config.MetricDefinition {
+	metadata := makeValidMetadata()
 	return config.MetricDefinition{
 		Id:         config_parser.IdFromName(name),
 		MetricName: name,
-		MetricType: config.MetricDefinition_EVENT_OCCURRED,
-		MetaData:   &config.MetricDefinition_Metadata{},
+		MetricType: config.MetricDefinition_EVENT_COUNT,
+		MetaData:   &metadata,
+		EventTypes: map[uint32]string{1: "hello_world"},
 	}
 }
 
@@ -45,6 +56,13 @@ func TestValidateMakeValidMetric(t *testing.T) {
 	m := makeValidMetric()
 	if err := validateMetricDefinition(m); err != nil {
 		t.Errorf("Rejected valid metric: %v", err)
+	}
+}
+
+func TestValidateMakeValidMetadata(t *testing.T) {
+	m := makeValidMetadata()
+	if err := validateMetadata(m); err != nil {
+		t.Errorf("Rejected valid metadata: %v", err)
 	}
 }
 
@@ -106,7 +124,7 @@ func TestValidateMaxEventTypeIndexOnlySetIfEventOccurred(t *testing.T) {
 	m.MetricType = config.MetricDefinition_EVENT_OCCURRED
 
 	if err := validateMetricDefinition(m); err != nil {
-		t.Error("Rejected valid metric definition with max_event_type_index set.")
+		t.Errorf("Rejected valid metric definition with max_event_type_index set: ", err)
 	}
 
 	for _, mt := range metricTypesExcept(config.MetricDefinition_EVENT_OCCURRED) {
@@ -117,16 +135,6 @@ func TestValidateMaxEventTypeIndexOnlySetIfEventOccurred(t *testing.T) {
 	}
 }
 
-// Test that max_event_type_index >= 1024 generates an error.
-func TestValidateMaxEventTypeIndexTooBig(t *testing.T) {
-	m := makeValidMetric()
-	m.MaxEventTypeIndex = 1024
-
-	if err := validateMetricDefinition(m); err == nil {
-		t.Error("Accepted metric definition with too large a max_event_type_index.")
-	}
-}
-
 // Test that int_buckets can only be set if the metric type is INT_HISTOGRAM.
 func TestValidateIntBucketsSetOnlyForIntHistogram(t *testing.T) {
 	m := makeValidMetric()
@@ -134,7 +142,7 @@ func TestValidateIntBucketsSetOnlyForIntHistogram(t *testing.T) {
 	m.MetricType = config.MetricDefinition_INT_HISTOGRAM
 
 	if err := validateMetricDefinition(m); err != nil {
-		t.Error("Rejected valid metric definition with int_buckets set.")
+		t.Errorf("Rejected valid INT_HISTOGRAM metric definition: %v", err)
 	}
 
 	for _, mt := range metricTypesExcept(config.MetricDefinition_INT_HISTOGRAM) {
@@ -148,11 +156,12 @@ func TestValidateIntBucketsSetOnlyForIntHistogram(t *testing.T) {
 // Test that parts can only be set if the metric type is CUSTOM.
 func TestValidatePartsSetOnlyForCustom(t *testing.T) {
 	m := makeValidMetric()
-	m.MetricType = config.MetricDefinition_CUSTOM
 	m.Parts = map[string]*config.MetricPart{"hello": nil}
+	m.MetricType = config.MetricDefinition_CUSTOM
+	m.EventTypes = map[uint32]string{}
 
 	if err := validateMetricDefinition(m); err != nil {
-		t.Error("Rejected valid metric definition with parts set.")
+		t.Errorf("Rejected valid CUSTOM metric definition: %v", err)
 	}
 
 	for _, mt := range metricTypesExcept(config.MetricDefinition_CUSTOM) {
@@ -170,5 +179,150 @@ func TestValidatePartsNoMetadata(t *testing.T) {
 
 	if err := validateMetricDefinition(m); err == nil {
 		t.Error("Accepted metric definition with no meta_data set.")
+	}
+}
+
+func TestValidateMetadataNoExpirationDate(t *testing.T) {
+	m := makeValidMetadata()
+	m.ExpirationDate = ""
+
+	if err := validateMetadata(m); err == nil {
+		t.Error("Accepted metadata with no expiration date.")
+	}
+}
+
+func TestValidateMetadataInvalidExpirationDate(t *testing.T) {
+	m := makeValidMetadata()
+	m.ExpirationDate = "abcd"
+
+	if err := validateMetadata(m); err == nil {
+		t.Error("Accepted invalid expiration date")
+	}
+}
+
+func TestValidateMetadataExpirationDateTooFar(t *testing.T) {
+	m := makeValidMetadata()
+	m.ExpirationDate = time.Now().AddDate(1, 0, 2).Format(dateFormat)
+
+	if err := validateMetadata(m); err == nil {
+		t.Errorf("Accepted expiration date more than 1 year out: %v", m.ExpirationDate)
+	}
+}
+
+func TestValidateMetadataExpirationDateInPast(t *testing.T) {
+	m := makeValidMetadata()
+	m.ExpirationDate = "2010/01/01"
+
+	if err := validateMetadata(m); err != nil {
+		t.Errorf("Rejected expiration date in the past: %v", err)
+	}
+}
+
+func TestValidateMetadataInvalidOwner(t *testing.T) {
+	m := makeValidMetadata()
+	m.Owner = append(m.Owner, "not a valid email")
+
+	if err := validateMetadata(m); err == nil {
+		t.Error("Accepted owner with invalid email address.")
+	}
+}
+
+func TestValidateMetadataReleaseStageNotSet(t *testing.T) {
+	m := makeValidMetadata()
+	m.MaxReleaseStage = config.ReleaseStage_RELEASE_STAGE_NOT_SET
+
+	if err := validateMetadata(m); err == nil {
+		t.Error("Accepted owner with no max_release_stage set.")
+	}
+}
+
+func TestValidateEventTypesMaxEventTypeIndexTooBig(t *testing.T) {
+	m := makeValidMetric()
+	m.MaxEventTypeIndex = 1024
+	m.EventTypes = map[uint32]string{
+		1: "hello_world",
+	}
+
+	if err := validateEventTypes(m); err == nil {
+		t.Error("Accepted max_event_type_index with value no less than 1024.")
+	}
+}
+
+func TestValidateEventTypesIndexLargerThanMax(t *testing.T) {
+	m := makeValidMetric()
+	m.MaxEventTypeIndex = 100
+	m.EventTypes = map[uint32]string{
+		1:   "hello_world",
+		101: "blah",
+	}
+
+	if err := validateEventTypes(m); err == nil {
+		t.Error("Accepted event type with index larger than max_event_type_index.")
+	}
+}
+
+func TestValidateEventTypesNoEventTypes(t *testing.T) {
+	m := makeValidMetric()
+	m.EventTypes = map[uint32]string{}
+
+	if err := validateEventTypes(m); err == nil {
+		t.Error("Accepted metric with no event types.")
+	}
+}
+
+func TestValidateEventOccurredNoMax(t *testing.T) {
+	m := makeValidMetric()
+	m.MaxEventTypeIndex = 0
+
+	if err := validateEventOccurred(m); err == nil {
+		t.Error("Accepted EVENT_OCCURRED metric with no max_event_type_index.")
+	}
+}
+
+func TestValidateIntHistogramNoBuckets(t *testing.T) {
+	m := makeValidMetric()
+	m.IntBuckets = nil
+
+	if err := validateIntHistogram(m); err == nil {
+		t.Error("Accepted INT_HISTOGRAM metric with no int_buckets.")
+	}
+}
+
+func TestValidateStringUsedEventTypesSet(t *testing.T) {
+	m := makeValidMetric()
+	m.EventTypes = map[uint32]string{1: "hello"}
+
+	if err := validateStringUsed(m); err == nil {
+		t.Error("Accepted STRING_USED metric with event_types set.")
+	}
+}
+
+func TestValidateCustomEventTypesSet(t *testing.T) {
+	m := makeValidMetric()
+	m.Parts = map[string]*config.MetricPart{"hello": nil}
+	m.EventTypes = map[uint32]string{1: "hello"}
+
+	if err := validateCustom(m); err == nil {
+		t.Error("Accepted CUSTOM metric with event_types set.")
+	}
+}
+
+func TestValidateCustomNoParts(t *testing.T) {
+	m := makeValidMetric()
+	m.EventTypes = map[uint32]string{}
+	m.Parts = map[string]*config.MetricPart{}
+
+	if err := validateCustom(m); err == nil {
+		t.Error("Accepted CUSTOM metric with no parts.")
+	}
+}
+
+func TestValidateCustomInvalidPartName(t *testing.T) {
+	m := makeValidMetric()
+	m.EventTypes = map[uint32]string{}
+	m.Parts = map[string]*config.MetricPart{"_invalid_name": nil}
+
+	if err := validateCustom(m); err == nil {
+		t.Error("Accepted CUSTOM metric with invalid part name.")
 	}
 }
